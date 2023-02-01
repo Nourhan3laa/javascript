e3a4391d177e15f69ca7edb388726bfeee285a89
Nourhan3laa <84689123+Nourhan3laa@users.noreply.github.com>
2023-02-01 17:17:28 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,224 @@
+// var salary = prompt('enter your salary');
+// var netsalary = (salary * 75) / 100;
+// console.log (netsalary);
+
+// var colors =['red','green','blue']
+// console.log(colors[0])
+// colors[3] = ('yellow')
+// console.log(colors)
+// var person = {}
+// person.firstname = 'ali'
+// person.work = 'nti'
+// console.log(person.work)
+// var details = [
+//     {fname : 'ahmed' , age : 30 , job :'php' ,
+//      projects: ['cars' ,'hospital']},
+
+//      {fname : 'ali' , age : 35 , job :'java' ,
+//      projects :['home' ,'chat app']},
+
+//      {fname : 'nour' , age : 22 , job :'ui-ux' ,
+//      projects :['travel' ,'home']},
+  
+// ]
+// console.log(details[1].projects[0]);
+
+// var person={firstname:'ali'}
+// var user = person
+// console.log(person.firstname);
+// console.log(user.firstname);
+// person.firstname = 'ahmed'
+// console.log(person.firstname);
+// console.log(user.firstname);
+// function report () {console.log('----start----');
+// console.log('------details--------');
+// console.log('------end-------');
+    
+// }
+ //report()
+//  function report(x,y) {
+//      var x ,y ,z;
+//      z= x + y
+//      alert(z)
+     
+//  }
+//  report(5,9)
+//  function report(x,y) {
+//      console.log('start' + x + 'work' + y);
+     
+//  }
+//  report(2022,'nti')
+// function ahmed() {
+//     var x =5
+//     var y =8
+//     var z =x+y
+//     return z
+    
+// }
+// console.log(ahmed());
+// function ali(x,y) {
+//     return x * y
+    
+// }
+// var m = ali (5,6)
+// console.log(m);
+// function total() {
+//     x = 40
+//     console.log(x);
+    
+// }
+// total()
+// console.log(x + 60);
+// 'use strict'
+// var s = 500
+// function total() {
+//     s = 300 
+//     console.log(s);
+    
+// }
+// total()
+// console.log(s);
+// var m1 =Number (prompt('enter x'))
+// var m2 =Number (prompt('enter y'))
+// function add() {
+//     var z = m1 + m2 
+//     console.log(z);
+  
+    
+// }
+// add()
+// function minous() {
+//     var z = m1 - m2
+//     console.log(z);
+    
+// }
+// minous()
+// var x = 10
+// x +=30
+// console.log(x);
+// x -=20
+// console.log(x);
+// x *=3
+// console.log(x);
+// x /=3
+// console.log(x);
+// var x = 20
+// var test= x>15? "yes greater than15" : "no less than"
+// console.log(test);
+// var x = 10
+// if(x > 5){
+// console.log('greater than');
+// }
+// else {
+//     console.log('no less than');
+// }
+
+// var salary =Number( prompt('enter your salary'))
+// var level =Number( prompt(' enter your level'))
+// if(2000 >= salary >= 5000){ netsalary = ('salary'*90)/100
+//     console.log('netsalary');
+
+
+// }
+// else if (5000 > salary > 10000) {
+//     console.log();
+    
+// }
+
+// var x = Number(prompt('enter your number'))
+// switch (x) {
+//     case 10:
+//     case 11 :
+//   console.log('x >= 10 ');    
+        
+//         break;
+//         case 12:
+//          console.log('x >= 12 ');
+//          break
+
+
+//     default: console.log('yes');
+//         break;
+// }
+// for ( i=1 ; i<5 ;i++ ) {
+//   console.log(i);
+    
+// }
+// var data = []
+// var data = Number (prompt('enter number'))
+// var toyal = 0
+
+// var price = Number (prompt('enter number'))
+// for (let i = 0; i < data ; i++) {
+    
+//     console.log(i);
+// }
+// for (let i = 0; i < price ; i++) {
+    
+//     console.log(i);
+// }
+// var x = ['red','green','yellow']
+// x[5] = 'cyan'
+// for (var i in x) {
+//     console.log(i);
+   
+// }
+// for (var i of x) {
+//     console.log(i);
+   
+// }
+// var x =0
+// while(x<5){
+//     console.log(x);
+//     x++
+// }
+// var x=5
+// do{
+//     console.log(x);
+//     x++
+// }while(x<5)
+
+// for(let i = 0; i < 10; i++){
+//     if(i == 3 || i == 5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+// var m = Number(prompt('enter number of product'))
+// var max = 0
+// for(i=0 ; i <m ; i++){
+//     var price = Number(prompt('enter price number of product'))
+//     if(price>max){
+//         max=price
+//     }
+// }
+// console.log('the max price is'+ max);
+// let x = prompt('enter your password')
+// if(x.length<6){
+//     console.log('your password weak');
+// }
+// else{
+//     console.log('your password good');
+// }
+// var x = 'hello'
+// var y = 'world'
+// var m = x.concat(y)
+// console.log(m);
+// var m = (prompt('enter your email'))
+// if (m.indexof('@' )) {
+//     console.log('m');
+    
+// }
+// else{
+//     console.log('enter valid mail');
+// }
+// var x = 'visit cairo visit cairo '
+// var y = x . search('cairo')
+// console.log(y);
+// var x = 'how are you doing today'
+// var y = x.split('')
+// console.log(y[5]);
+// var x = document.getElementsByClassName('nti')
+// console.log(x);
+
+
